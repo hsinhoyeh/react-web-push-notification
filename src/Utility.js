639bbd17c63618babb83b4e184c6b0024ab0df88
hsinhoyeh <yhh92u@gmail.com>
2017-05-25 18:54:37 +0800

    move utility functions to utility.js

--- src/Utility.js
+++ src/Utility.js
@@ -12,4 +12,21 @@ const payloadFromSubscription = function(subscription) {
   }
 }
 
+const urlB64ToUint8Array= function(base64String) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4)
+  const base64 = (base64String + padding)
+    .replace(/\-/g, '+')
+    .replace(/_/g, '/')
+
+  const rawData = window.atob(base64)
+  const outputArray = new Uint8Array(rawData.length)
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i)
+  }
+  return outputArray
+}
+
+
 exports.payloadFromSubscription = payloadFromSubscription
+exports.urlB64ToUint8Array = urlB64ToUint8Array
